5fbcc916d8784f5b354a5c5aea73492c71e7655e
Alexandre Fleurquin <alexandre.fleurquin@gmail.com>
2022-08-15 20:20:01 +0200

    Longest Word Game V2

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,31 +4,26 @@ require "open-uri"
 class GamesController < ApplicationController
   def new
     @letters = []
-    10.times { @letters << ("A".."Z").to_a.sample }
+    10.times { @letters << ('A'..'Z').to_a.sample }
     @letters
   end
 
-  def letter_in_grid?
-    @answer.chars.sort.all? { |letter| @grid.include?(letter) }
-  end
-
-  def english_word?
-    url = "https://wagon-dictionary.herokuapp.com/#{@answer}"
-    word_dictionary = open(url).read
-    word = JSON.parse(word_dictionary)
-    word['found']
-  end
-
   def score
-    @grid = params[:grid]
-    @answer = params[:word]
-    grid_letters = @grid.each_char { |letter| print letter, '' }
-    if !letter_in_grid?
-      @score = "Sorry, but #{@answer} can’t be built out of #{grid_letters}."
-    elsif !english_word?
-      @score = "Sorry but #{@answer} does not seem to be an English word."
-    else
-      @score = "Congratulation! #{@answer} is a valid English word."
-    end
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
